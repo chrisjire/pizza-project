abc9b17fcebf58e05f75111ad88bc80948731313
chris <christianwilliamsjire@gmail.com>
2019-07-29 11:37:11 +0300

    this commit will add checkout form to pepperoni pizza

--- js/script.js
+++ js/script.js
@@ -1,7 +1,40 @@
-function Dog(size, colors, age) {
-    this.size = size;
-    this.crust = crust;
-    this.toppings = toppings;
-}  
+function getSizeValue() {
+    var selectedValue = document.getElementById("size").value;
+    return parseInt(selectedValue);
+}
 
-//front end
+function getCrust() {
+    var selectedCrust = document.getElementById("toppings").value;
+    return parseInt(selectedCrust);
+}
+
+
+function getToppings() {
+    var selectedToppings = document.getElementById("toppings").value;
+    return parseInt(selectedToppings);
+}
+
+function getQuantity() {
+    var selectedQuantity = document.getElementById("quan").value;
+    return parseInt(selectedQuantity);
+}
+
+function getTotalAmount() {
+  var totalAmount = (getSizeValue() + getCrust() + getToppings()) * getQuantity();
+    alert("You ordered " + getQuantity() + " pizza(s)." + ""+" Your bill is " + (totalAmount));
+}
+
+
+//UI interface
+$(document).ready(function() {
+    $("#select").click(function() {
+      $("#select").show();
+      $("#hform").hide();
+    });
+  });
+  $(document).ready(function() {
+    $("#select").click(function() {
+      $("#hform").show();
+      $("#select").hide();
+    });
+  });
